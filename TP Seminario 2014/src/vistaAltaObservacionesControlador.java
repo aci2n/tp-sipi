d31655297146c1a607a6aea601552b4c4829e178
Mariano <Mariano@Mariano-PC>
2014-12-07 23:43:07 -0300

    a

--- TP Seminario 2014/src/vistaAltaObservacionesControlador.java
+++ TP Seminario 2014/src/vistaAltaObservacionesControlador.java
@@ -1,5 +1,16 @@
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class vistaAltaObservacionesControlador {
+import javafx.fxml.Initializable;
+
+
+public class vistaAltaObservacionesControlador implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
 	
 
 }
